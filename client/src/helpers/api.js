3e4030ff924b1870d6e9e58a3e74b9bf80db43a1
Tamas Vigh <tamas.vigh@rebilly.com>
2024-09-26 13:54:33 +0200

    update server uri

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,7 +1,12 @@
 import { useUserStore } from '@/store'
 
+const url = import.meta.env.VITE_IS_DEV === 'true'
+  ? 'http://localhost:3000'
+  : 'https://phronesis-sun-dubai.onrender.com';
+
 async function api({ method, resource, body }) {
-  const response = await fetch(`http://localhost:3000/${resource}`, {
+
+  const response = await fetch(`${url}/${resource}`, {
     method,
     ...(method !== 'GET' && { body: JSON.stringify(body) }),
     headers: {
