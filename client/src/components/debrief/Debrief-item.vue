e2c87ce9012597c2ebda511396bb44d4c875d9b0
Ikram Patah <ikram.patah@rebilly.com>
2024-09-27 17:07:34 +0400

    Add debrief item component

---
+++ client/src/components/debrief/Debrief-item.vue
@@ -0,0 +1,40 @@
+<script setup>
+const props = defineProps({
+  id: String
+})
+</script>
+
+<template>
+  <div class="accordion" id="accordion-debrief">
+    <div class="accordion-item">
+      <h2 class="accordion-header">
+        <button
+          class="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          :data-bs-target="`#debrief-${props.id}`"
+        >
+          <strong>
+            <slot name="title"></slot>
+          </strong>
+        </button>
+      </h2>
+      <div
+        :id="`debrief-${props.id}`"
+        class="accordion-collapse collapse show"
+        data-bs-parent="#accordion-debrief"
+      >
+        <div class="accordion-body">
+          <h6 class="pb-3">
+            <strong>What made you smile?</strong>
+          </h6>
+          <slot name="smile"></slot>
+          <h6>
+            <strong>What did you find confusing?</strong>
+          </h6>
+          <slot name="confusing"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- client/src/components/debrief/Debriefs.vue
+++ client/src/components/debrief/Debriefs.vue
@@ -1,27 +1,45 @@
+<script setup>
+import DebriefItem from '@/components/debrief/Debrief-item.vue'
+</script>
+
 <template>
+  <div class="py-5 bg-body-tertiary">
     <div class="container">
-        <h2 class="pb-5 pt-2">Debriefs</h2>
-        <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Project 8: Add consent check to the payment flow
-                    </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        <h3 class="pb-3">What made you smile?</h3>
-                        <p>Easy to add to existing configuration. In the docs this feature has its own page, which makes it easy to follow (rather than just a reference). </p>
-                        <h3 class="pb-3">What did you find confusing?</h3>
-                        <ul>
-                            <li class="list-item">It would be nice to have a default value if the feature is turned on without any additional i18n texting.</li>
-                            <li class="list-item">The docs could show an image preview of the feature.</li>
-                            <li class="list-item">Copiable code on their own should work out of the box. Now it does not work without the i18n code.</li>
-                            <li class="list-item">The links' styles are not consistent with the rest of instruments links (use primaryColor?)</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div class="text-center mb-4">
+        <h2>Debriefs</h2>
+      </div>
     </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <DebriefItem id="week-8">
+            <template #title>Project 8: Add consent check to the payment flow</template>
+            <template #smile>
+              <p>
+                Easy to add to existing configuration. In the docs this feature has its own page,
+                which makes it easy to follow (rather than just a reference).
+              </p>
+            </template>
+            <template #confusing>
+              <ul>
+                <li class="list-item">
+                  It would be nice to have a default value if the feature is turned on without any
+                  additional i18n texting.
+                </li>
+                <li class="list-item">The docs could show an image preview of the feature.</li>
+                <li class="list-item">
+                  Copiable code on their own should work out of the box. Now it does not work
+                  without the i18n code.
+                </li>
+                <li class="list-item">
+                  The links' styles are not consistent with the rest of instruments links (use
+                  primaryColor?)
+                </li>
+              </ul>
+            </template>
+          </DebriefItem>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
